89fa5e4b88391c9da827457d89c04996eb48d224
Rogerndaba <rogerndaba@gmail.com>
2020-05-15 13:40:38 +0200

    implementing threaded NIO

--- router/src/main/java/com/fixme/Router.java
+++ router/src/main/java/com/fixme/Router.java
@@ -1,5 +1,20 @@
 package com.fixme;
 
+import com.fixme.RouterHandler;
+
 public class Router {
+	public static void main(String[] args) {
+		RouterHandler rh1 = new RouterHandler("broker", 5000);
+		RouterHandler rh2 = new RouterHandler("market", 5001);
+
+		rh1.setHandler(rh2);
+		rh2.setHandler(rh1);
+
+		Thread t1 = new Thread(rh1);
+		Thread t2 = new Thread(rh2);
+
+		t1.start();
+		t2.start();
+	}
 
 }
--- router/src/main/java/com/fixme/RouterHandler.java
+++ router/src/main/java/com/fixme/RouterHandler.java
@@ -3,9 +3,28 @@ package com.fixme;
 public class RouterHandler implements Runnable {
 
 	private int port;
+	private String name;
+	private RouterHandler client;
 
-	public RouterHandler(int _port) {
+	public RouterHandler(String _name, int _port) {
 		this.port = _port;
+		this.name = _name;
+	}
+
+	public void run() {
+		System.out.println("Port : " + this.port);
+		while (true) {
+			System.out.println("*** " + this.name + " ***");
+		}
+	}
+
+	public void setHandler(RouterHandler _handler) {
+		this.client = _handler;
+		System.out.println("This is : " + _handler.getName());
+	}
+
+	public String getName() {
+		return this.name;
 	}
 
 }
