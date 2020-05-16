6c5214dc2f09202e11d92e2e4da5c11ba358b7c7
Rogerndaba <rogerndaba@gmail.com>
2020-05-16 14:22:55 +0200

    working on broker login

--- broker/src/main/java/com/fixme/Broker.java
+++ broker/src/main/java/com/fixme/Broker.java
@@ -1,62 +1,80 @@
 package com.fixme;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
+import com.fixme.Colour;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+// import java.io.IOException;
+// import java.net.InetSocketAddress;
+// import java.nio.ByteBuffer;
+// import java.nio.channels.SocketChannel;
 
 public class Broker {
-	private static SocketChannel client;
-	private String ConnID;
-	private static ByteBuffer buffer;
-	private static Broker instance;
+	private Scanner scn;
+	private String ConnID = "";
+	private HashMap<String, Object> details;
 
-	public static Broker start() {
-		if (instance == null)
-			instance = new Broker();
+	// private static SocketChannel client;
+	// private String ConnID;
+	// private static ByteBuffer buffer;
+	// private static Broker instance;
 
-		return instance;
+	public Broker() {
+		this.scn = new Scanner(System.in);
 	}
 
-	public static void stop() throws IOException {
-		client.close();
-		buffer = null;
-	}
-
-	private Broker() {
-		try {
-			client = SocketChannel.open(new InetSocketAddress("localhost", 5000));
-			buffer = ByteBuffer.allocate(256);
-
-			buffer.clear();
-			client.read(buffer);
-			String response = new String(buffer.array()).trim();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public String sendMessage(String msg) {
-		buffer = ByteBuffer.wrap(msg.getBytes());
-		String response = null;
-		try {
-			client.write(buffer);
-			buffer.clear();
-			client.read(buffer);
-			response = new String(buffer.array()).trim();
-			System.out.println("response=" + response);
-			buffer.clear();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return response;
+	public boolean login() {
+		Colour.white("Please enter broker username: ");
+		String username = this.scn.nextLine().trim();
+		Colour.white("Please enter broker username: ");
+		String password = this.scn.nextLine().trim();
 
 	}
+
+	// public static void stop() throws IOException {
+	// client.close();
+	// buffer = null;
+	// }
+
+	// private Broker() {
+	// try {
+	// client = SocketChannel.open(new InetSocketAddress("localhost", 5000));
+	// buffer = ByteBuffer.allocate(256);
+
+	// buffer.clear();
+	// client.read(buffer);
+	// String response = new String(buffer.array()).trim();
+
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	// public String sendMessage(String msg) {
+	// buffer = ByteBuffer.wrap(msg.getBytes());
+	// String response = null;
+	// try {
+	// client.write(buffer);
+	// buffer.clear();
+	// client.read(buffer);
+	// response = new String(buffer.array()).trim();
+	// System.out.println("response=" + response);
+	// buffer.clear();
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// return response;
+
+	// }
 
 	public static void main(String[] args) {
-		Broker br = Broker.start();
-		br.sendMessage("new");
+		Scanner scanner = new Scanner(System.in);
+
+		Colour.out.green("\n\tWELCOME TO THE FIX PORTAL\n");
+
+		// Broker br = Broker.start();
+		// br.sendMessage("new");
 
 		// try {
 
--- resources/src/main/java/com/fixme/Colour.java
+++ resources/src/main/java/com/fixme/Colour.java
@@ -18,6 +18,18 @@ public class Colour {
 		}
 	}
 
+	static public void red(String s) {
+		System.out.print("\u001B[1;31m" + s + "\u001B[0m");
+	}
+
+	static public void green(String s) {
+		System.out.print("\u001B[1;32m" + s + "\u001B[0m");
+	}
+
+	static public void white(String s) {
+		System.out.print("\u001B[1;37m" + s + "\u001B[0m");
+	}
+
 	static public class line {
 		static public String red(String s) {
 			return ("\u001B[1;31m" + s + "\u001B[0m");
